13eaf11195f7e3dfc2767fabe3a7f19407699ca8
Yusuf <ahmadyusuf@uinsa.ac.id>
2026-04-09 10:04:04 +0700

    perubahan config db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pendaftaran',
+    'dsn' => 'mysql:host=localhost;dbname=registrasi',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
